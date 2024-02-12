bf6c9f5e7ff3624e559624991530024c64deb1b3
Amin Faruq <aminfaruk.fa@gmail.com>
2024-02-12 23:00:53 +0800

    move to production code

--- QuizEngine/QuizEngine/Flow.swift
+++ QuizEngine/QuizEngine/Flow.swift
@@ -7,3 +7,45 @@
 
 import Foundation
 
+protocol Router {
+    typealias AnswerCallback = (String) -> Void
+    func routeTo(question: String, answerCallback: @escaping AnswerCallback)
+    func routeTo(result: [String: String])
+}
+
+class Flow {
+    private let router: Router
+    private let questions: [String]
+    private var result: [String : String] = [:]
+    
+    init(questions: [String], router: Router) {
+        self.questions = questions
+        self.router = router
+    }
+    
+    func start() {
+        if let firstQuestions = questions.first {
+            router.routeTo(question: firstQuestions, answerCallback: nextCallback(from: firstQuestions))
+        } else {
+            router.routeTo(result: result)
+        }
+    }
+    
+    private func nextCallback(from question: String) -> Router.AnswerCallback {
+        return { [weak self] in self?.routeNext(question, $0)}
+    }
+    
+    private func routeNext(_ question: String, _ answer: String) {
+        if let currentQuestionIndex = questions.firstIndex(of: question) {
+            result[question] = answer
+            
+            let nextQuestionIndex = currentQuestionIndex + 1
+            if nextQuestionIndex < questions.count {
+                let nextQuestion = questions[nextQuestionIndex]
+                router.routeTo(question: nextQuestion, answerCallback: nextCallback(from: nextQuestion))
+            } else {
+                router.routeTo(result: result)
+            }
+        }
+    }
+}

2e48e32646b2089f5b54826a73d4c7da73c975e6
Amin Faruq <aminfaruk.fa@gmail.com>
2024-02-15 00:44:59 +0800

    End of Tomato 6 - Refactoring + Question selection

--- QuizApp/QuizAppTests/QuestionViewControllerTest.swift
+++ QuizApp/QuizAppTests/QuestionViewControllerTest.swift
@@ -26,17 +26,33 @@ final class QuestionViewControllerTest: XCTestCase {
     }
     
     func test_viewDidLoad_rendersOptionsText() {
-        var sut = makeSUT(options: ["A1", "A2"])
+        let sut = makeSUT(options: ["A1", "A2"])
         
         XCTAssertEqual(sut.tableView.title(at: 0), "A1")
         XCTAssertEqual(sut.tableView.title(at: 1), "A2")
     }
     
+    func test_optionSelected_notifiesDelegate() {
+        var receivedAnswer = ""
+        
+        let sut = makeSUT(options: ["A1"]) {
+            receivedAnswer = $0
+        }
+                
+        let indexPath = IndexPath(row: 0, section: 0)
+        sut.tableView.delegate?.tableView?(sut.tableView, didSelectRowAt: indexPath)
+        
+        XCTAssertEqual(receivedAnswer, "A1")
+    }
+    
     //MARK: - Helpers
     
-    func makeSUT(question: String = "", options: [String] = []) -> QuestionViewController {
-        let sut = QuestionViewController(question: question, options: options)
-        //_ = sut.view
+    func makeSUT(
+        question: String = "",
+        options: [String] = [],
+        selection: @escaping (String) -> Void = { _ in }
+    ) -> QuestionViewController {
+        let sut = QuestionViewController(question: question, options: options, selection: selection)
         
         sut.loadViewIfNeeded()
         return sut

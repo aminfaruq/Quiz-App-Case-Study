6c1f1cb7615e24d828200876418a16a792994055
Amin Faruq <aminfaruk.fa@gmail.com>
2024-02-24 13:33:29 +0800

    S01E20: [TDD 🍅 17] Testing Presenters in Swift pt.1

--- QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
+++ QuizApp/QuizAppTests/NavigationControllerRouterTest.swift
@@ -93,12 +93,3 @@ final class NavigationControllerRouterTest: XCTestCase {
     }
 }
 
-extension Result: Hashable {
-    public func hash(into hasher: inout Hasher) {
-        hasher.combine(1)
-    }
-    
-    public static func ==(lhs: Result<Question, Answer>, rhs: Result<Question, Answer>) -> Bool {
-        return lhs.score == rhs.score
-    }
-}

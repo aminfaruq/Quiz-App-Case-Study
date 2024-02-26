f7cc0e090585cb68df28e41c90a4dce05f3684d8
Amin Faruq <aminfaruk.fa@gmail.com>
2024-02-26 19:43:12 +0800

    remove unused code

--- QuizEngine/QuizEngine/Game.swift
+++ QuizEngine/QuizEngine/Game.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public class Game<Question: Hashable, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
+public class Game<Question, Answer, R: Router> where R.Question == Question, R.Answer == Answer {
     
     let flow: Flow<Question, Answer, R>
     
@@ -16,14 +16,14 @@ public class Game<Question: Hashable, Answer, R: Router> where R.Question == Que
     }
 }
 
-public func startGame<Question: Hashable, Answer: Equatable, R: Router>(questions: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
+public func startGame<Question, Answer: Equatable, R: Router>(questions: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> {
     let flow = Flow(questions: questions, router: router, scoring: { scoring($0, correctAnswers: correctAnswers) })
     
     flow.start()
     return Game(flow: flow)
 }
 
-private func scoring<Question: Hashable, Answer: Equatable>(_ answer: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
+private func scoring<Question, Answer: Equatable>(_ answer: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
     return answer.reduce(0) { (score, tuple) in
         return score + (correctAnswers[tuple.key] == tuple.value ? 1 : 0)
     }

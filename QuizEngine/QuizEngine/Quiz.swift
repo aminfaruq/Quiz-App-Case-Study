f4d5ff52e8718842cecb71eee4376241e981c35b
Amin Faruq <aminfaruk.fa@gmail.com>
2024-03-01 10:13:03 +0800

    Move Quiz to its own file in production

---
+++ QuizEngine/QuizEngine/Quiz.swift
@@ -0,0 +1,22 @@
+//
+//  Quiz.swift
+//  QuizEngine
+//
+//  Created by Amin faruq on 01/03/24.
+//
+
+import Foundation
+
+public final class Quiz {
+    private let flow: Any
+
+    private init(flow: Any) {
+        self.flow = flow
+    }
+
+    public static func start<Question, Answer: Equatable, Delegate: QuizDelegate>(questions: [Question], delegate: Delegate, correctAnswers: [Question: Answer]) -> Quiz where Delegate.Question == Question, Delegate.Answer == Answer {
+        let flow = Flow(questions: questions, delegate: delegate, scoring: { scoring($0, correctAnswers: correctAnswers) })
+        flow.start()
+        return Quiz(flow: flow)
+    }
+}

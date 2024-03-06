7f02fed942ecc8b800171cbc279fc6ebb3cee2f4
Amin Faruq <aminfaruk.fa@gmail.com>
2024-03-07 00:31:37 +0800

    scores on a correct answers

--- QuizEngine/QuizEngineTests/ScoreTest.swift
+++ QuizEngine/QuizEngineTests/ScoreTest.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 final class ScoreTest: XCTestCase {
-
+    
     func test_noAnswers_scoresZero() {
         XCTAssertEqual(BasicScore.score(for: [], comparingTo: []), 0)
     }
@@ -17,9 +17,14 @@ final class ScoreTest: XCTestCase {
         XCTAssertEqual(BasicScore.score(for: ["wrong"], comparingTo: ["correct"]), 0)
     }
     
+    func test_oneCorrectAnswer_scoresOne() {
+        XCTAssertEqual(BasicScore.score(for: ["correct"], comparingTo: ["correct"]), 1)
+    }
+    
     private class BasicScore{
-        static func score(for: [Any], comparingTo: [Any]) -> Int {
-            return 0
+        static func score(for answers: [String], comparingTo correctAnswers: [String]) -> Int {
+            if answers.isEmpty { return 0 }
+            return answers == correctAnswers ? 1 : 0
         }
     }
 }

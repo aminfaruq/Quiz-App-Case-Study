44416e775c74d1998193c6d865c8369a8184ea1d
Amin Faruq <aminfaruk.fa@gmail.com>
2024-03-10 15:09:09 +0800

    Implemented the didCompleteQuiz method in preparation to replace the deprecated Router implementation

--- QuizApp/QuizAppTests/Routing/NavigationControllerRouterTest.swift
+++ QuizApp/QuizAppTests/Routing/NavigationControllerRouterTest.swift
@@ -119,17 +119,16 @@ final class NavigationControllerRouterTest: XCTestCase {
     
     func test_routeToResult_showsResultController() {
         let viewController = UIViewController()
-        let result = Result(answers: [singleAnswerQuestion: ["A1"]], score: 10)
+        let userAnswers = [(singleAnswerQuestion, ["A1"])]
         
         let secondViewController = UIViewController()
-        let secondResult = Result(answers: [multipleAnswerQuestion: ["A2"]], score: 20)
+        let secondUserAnswers = [(multipleAnswerQuestion, ["A2"])]
         
-        factory.stub(result: result, with: viewController)
-        factory.stub(result: secondResult, with: secondViewController)
+        factory.stub(resultForQuestions: [singleAnswerQuestion], with: viewController)
+        factory.stub(resultForQuestions: [multipleAnswerQuestion], with: secondViewController)
 
-        sut.routeTo(result: result)
-        sut.routeTo(result: secondResult)
-
+        sut.didCompleteQuiz(withAnswers: userAnswers)
+        sut.didCompleteQuiz(withAnswers: secondUserAnswers)
 
         XCTAssertEqual(navigationController.viewControllers.count, 2)
         XCTAssertEqual(navigationController.viewControllers.first, viewController)
@@ -147,7 +146,7 @@ final class NavigationControllerRouterTest: XCTestCase {
     class ViewControllerFactoryStub: ViewControllerFactory {
         
         private var stubbedQuestions = Dictionary<Question<String>, UIViewController>()
-        private var stubbedResults = Dictionary<Result<Question<String>, [String]>, UIViewController>()
+        private var stubbedResults = Dictionary<[Question<String>], UIViewController>()
 
         var answerCallback = Dictionary<Question<String>, ([String]) -> Void>()
         
@@ -155,8 +154,8 @@ final class NavigationControllerRouterTest: XCTestCase {
             stubbedQuestions[question] = viewController
         }
         
-        func stub(result: Result<Question<String>, [String]>, with viewController: UIViewController) {
-            stubbedResults[result] = viewController
+        func stub(resultForQuestions questions: [Question<String>], with viewController: UIViewController) {
+            stubbedResults[questions] = viewController
         }
         
         func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
@@ -165,11 +164,11 @@ final class NavigationControllerRouterTest: XCTestCase {
         }
         
         func resultsViewController(for userAnswers: Answers) -> UIViewController {
-            return UIViewController()
+            return stubbedResults[userAnswers.map { $0.question } ] ?? UIViewController()
         }
         
         func resultsViewController(for result: Result<Question<String>, [String]>) -> UIViewController {
-            return stubbedResults[result] ?? UIViewController()
+            return UIViewController()
         }
     }
 }

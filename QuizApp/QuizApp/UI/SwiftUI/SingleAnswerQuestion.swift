956031f490ead981cb32b1bec3a206aea657879e
Amin Faruq <aminfaruk.fa@gmail.com>
2024-03-12 16:20:50 +0800

    feat: Add Interactable  SingleAnswerQuestionTestView preview

--- QuizApp/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
+++ QuizApp/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -19,7 +19,7 @@ struct SingleAnswerQuestion: View {
             
             ForEach(options, id: \.self) { option in
                 SingleTextSelectionCell(text: option, selection: {
-                    
+                    selection(option)
                 })
             }
             
@@ -29,31 +29,33 @@ struct SingleAnswerQuestion: View {
 }
 
 #Preview {
-    SingleAnswerQuestion(
-        title: "1 of 2",
-        question: "What's Mike's nationality?",
-        options: [
-            "Portuguese",
-            "American",
-            "Greek",
-            "Canadian"
-        ],
-        selection: { _ in }
-    )
+    SingleAnswerQuestionTestView()
 }
 
 #Preview {
-    SingleAnswerQuestion(
-        title: "1 of 2",
-        question: "What's Mike's nationality?",
-        options: [
-            "Portuguese",
-            "American",
-            "Greek",
-            "Canadian"
-        ],
-        selection: { _ in }
-    )
-    .preferredColorScheme(.dark)
-    .environment(\.sizeCategory, .extraExtraExtraLarge)
+    SingleAnswerQuestionTestView()
+        .preferredColorScheme(.dark)
+        .environment(\.sizeCategory, .extraExtraExtraLarge)
 }
+
+struct SingleAnswerQuestionTestView: View {
+    @State var selection: String = "none"
+    var body: some View {
+        VStack {
+            SingleAnswerQuestion(
+                title: "1 of 2",
+                question: "What's Mike's nationality?",
+                options: [
+                    "Portuguese",
+                    "American",
+                    "Greek",
+                    "Canadian"
+                ],
+                selection: { selection = $0 }
+            )
+            
+            Text("Last selection " + selection)
+        }
+    }
+}
+

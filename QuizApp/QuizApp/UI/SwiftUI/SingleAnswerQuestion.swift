3f7b14f3408182a00926fb9b1d06b38cc152ddd2
Amin Faruq <aminfaruk.fa@gmail.com>
2024-03-12 15:16:23 +0800

    Add SingleAnswerQuestion

---
+++ QuizApp/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -0,0 +1,81 @@
+//
+//  SingleAnswerQuestion.swift
+//  QuizApp
+//
+//  Created by Amin faruq on 12/03/24.
+//
+
+import SwiftUI
+
+struct SingleAnswerQuestion: View {
+    let title: String
+    let question: String
+    let options: [String]
+    let selection: (String) -> Void
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0.0) {
+            VStack(alignment: .leading, spacing: 16.0) {
+                Text(title)
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.blue)
+                    .padding(.top)
+                
+                Text(question)
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+            }
+            .padding()
+            
+            ForEach(options, id: \.self) { option in
+                Button (action: {
+                    
+                }, label: {
+                    HStack {
+                        Circle()
+                            .stroke(Color.secondary, lineWidth: 2.5)
+                        .frame(width: 40.0, height: 40.0)
+                        
+                        Text(option)
+                            .font(.title)
+                            .foregroundStyle(Color.secondary)
+                        
+                        Spacer()
+                    }.padding()
+                })
+            }
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SingleAnswerQuestion(
+        title: "1 of 2",
+        question: "What's Mike's nationality?",
+        options: [
+            "Portuguese",
+            "American",
+            "Greek",
+            "Canadian"
+        ],
+        selection: { _ in }
+    )
+}
+
+#Preview {
+    SingleAnswerQuestion(
+        title: "1 of 2",
+        question: "What's Mike's nationality?",
+        options: [
+            "Portuguese",
+            "American",
+            "Greek",
+            "Canadian"
+        ],
+        selection: { _ in }
+    )
+    .preferredColorScheme(.dark)
+    .environment(\.sizeCategory, .extraExtraExtraLarge)
+}
